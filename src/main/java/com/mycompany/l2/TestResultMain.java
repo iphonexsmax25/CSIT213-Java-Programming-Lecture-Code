73ab51acf8c7258797eb36c5859e7ffe6d818aa1
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-06 09:58:40 +0800

    add

--- src/main/java/com/mycompany/l2/TestResultMain.java
+++ src/main/java/com/mycompany/l2/TestResultMain.java
@@ -13,11 +13,18 @@ public class TestResultMain {
         System.out.println(TestResult.getP1Weighting());
         TestResult tr1 = new TestResult ("s1", 50, 50);
         TestResult tr2 = new TestResult ("s2", 30, 50);
-        // Call instances methods 
+        // Call instances methods
         System.out.println(tr1.getOverallScore());
         System.out.println(tr2.getOverallScore());
+        
+        
         //Printing instances
         System.out.println(tr1);
         System.out.println(tr2);
+        
+        
+        // Change the weightings
+        TestResult.setWeightings(0.4, 0.6);
+        System.out.println(TestResult.getP1Weighting());
     }
 }
