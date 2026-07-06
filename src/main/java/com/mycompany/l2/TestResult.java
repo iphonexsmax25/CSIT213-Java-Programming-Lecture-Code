bff1a611710a954c6e755f2fc5ccc893c5b78fa2
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-06 09:26:36 +0800

    add

--- src/main/java/com/mycompany/l2/TestResult.java
+++ src/main/java/com/mycompany/l2/TestResult.java
@@ -9,5 +9,16 @@ package com.mycompany.l2;
  * @author limis
  */
 public class TestResult {
-    
+    private String ID;
+    private double paper1, paper2;
+    private static double p1Weighting = 0.3;
+    private static double p2Weighting = 0.7;
+    public TestResult(String sID, double score1, double score2){
+        ID = sID;
+        paper1 = score1;
+        paper2 = score2;
+    }
+    public static double getP1Weighting(){
+        return p1Weighting;
+    }
 }
