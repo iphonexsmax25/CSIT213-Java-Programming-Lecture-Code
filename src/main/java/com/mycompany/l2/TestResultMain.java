b48626fbff3257702bd4676a522a1349b723490c
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-06 10:04:19 +0800

    add

--- src/main/java/com/mycompany/l2/TestResultMain.java
+++ src/main/java/com/mycompany/l2/TestResultMain.java
@@ -12,7 +12,7 @@ public class TestResultMain {
      public static void main(String[] args){
         System.out.println(TestResult.getP1Weighting());
         TestResult tr1 = new TestResult ("s1", 50, 50);
-        TestResult tr2 = new TestResult ("s2", 30, 50);
+        TestResult tr2 = new TestResult ("s2", 30, 80);
         // Call instances methods
         System.out.println(tr1.getOverallScore());
         System.out.println(tr2.getOverallScore());
