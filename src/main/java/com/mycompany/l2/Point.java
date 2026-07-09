fadcb175c1f20ea4e2f0633db0d7a1eb2584dcd2
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-09 16:35:45 +0800

    works

--- src/main/java/com/mycompany/l2/Point.java
+++ src/main/java/com/mycompany/l2/Point.java
@@ -3,34 +3,41 @@ package com.mycompany.l2;
 
 
 public class Point {
+
     private double x, y;
-    public Point(){
-        x = 1;
-        y = 1; 
+
+    public Point() {
+
     }
-    
-    public Point(double xCoord, double yCoord){
+
+    //Constructor
+    public Point(double xCoord, double yCoord) {
         x = xCoord;
         y = yCoord;
     }
-    
-    public double getDistance(){
-        double sumSq = x * x + y * y; 
-        return Math.sqrt(sumSq);
+
+    public double getDistance() {
+
+        double sumSqq = x * x + y * y;
+        return Math.sqrt(sumSqq);
     }
-    
-    //p1.getDisance(p3)
-     public double getDistance(Point other){
-         double xDiff = x - other.x;
-         double yDiff = y - other.y;
-         return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+
+    public double getDistance(Point other) {
+        double xDiff = x - other.x;
+        double yDiff = y - other.y;
+        //x is p1.x
+        // other.x is -3.x
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
     }
-    
-     public boolean equals (Point other){
-         return false;
-     }
-    public String toString(){
-        return String.format("(%.1f,  %.1f)", x, y);
+
+    public boolean equals(Point other) {
+        if (other == null) {
+            return false;
+        }
+        return (x == other.x && y == other.y);
     }
-    
+
+    public String toString() {
+        return String.format("(%.1f,%.1f)", x, y);
+    }
 }
