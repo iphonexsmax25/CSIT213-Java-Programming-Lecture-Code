1b5b004b9a449364e9fd92c55b2809efe37e9418
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-20 17:20:55 +0800

    works

---
+++ src/main/java/AboutArrayList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class AboutArrayList {
+    public static void main (String[] args){
+        
+    }
+    
+    public static void test1(){
+        ArrayList aList = new ArrayList();
+        System.out.println(aList.size());
+    }
+}
