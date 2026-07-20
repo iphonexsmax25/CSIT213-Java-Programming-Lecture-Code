3127aa7eec0c6d42f1cf7b980292a109d1cd99d7
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-20 15:59:37 +0800

    works

--- src/main/java/FractionTest.java
+++ src/main/java/FractionTest.java
@@ -2,6 +2,17 @@ public class FractionTest {
     public static void main(String[] argv){
         testFraction();
         testEquals();
+        testAddMethod();
+    }
+    public static void testAddMethod(){
+        Fraction f1 = new Fraction(1,10);
+        Fraction f2 = new Fraction(2,10);
+        Fraction f3 = new Fraction(3,10);
+        //
+        Fraction sum1 = f1.add(f2);
+        System.out.println(sum1);
+        System.out.println(f1.add(f3));
+        
     }
     public static void testEquals(){
         Fraction f1 = new Fraction(1, 10);
