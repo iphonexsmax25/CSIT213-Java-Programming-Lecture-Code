7800103719777b59de60048799c96267bc3bed3d
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-20 17:25:58 +0800

    works

--- src/main/java/AboutArrayList.java
+++ src/main/java/AboutArrayList.java
@@ -1,11 +1,21 @@
 import java.util.ArrayList;
 public class AboutArrayList {
     public static void main (String[] args){
-        
+        test1();
     }
     
     public static void test1(){
         ArrayList aList = new ArrayList();
-        System.out.println(aList.size());
+        System.out.println(aList.size()); // 0
+        aList.add("first item");
+        aList.add(123);
+        aList.add(12.34);
+        aList.add(false);
+        System.out.println(aList.size()); // 4
+        
+        // Use the get method + loop
+        for(int i = 0;i < aList.size(); i++){
+            
+        }
     }
 }
