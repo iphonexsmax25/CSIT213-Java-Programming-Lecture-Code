cbb57c624801fed9e91ad38b6428336e9b935af0
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-03 17:43:14 +0800

    works

--- src/main/java/about_abstarct_class/ResultTest.java
+++ src/main/java/about_abstarct_class/ResultTest.java
@@ -15,6 +15,7 @@ public class ResultTest {
     public static void test1(){
         OralResult r1 = new OralResult("test 1", "c1", 60, 60, 60);
         System.out.println(r1.getFinalScore());
+        System.out.println(r1.getGrade());
         System.out.println(r1);
     }
 }
