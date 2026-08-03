1eadf2689856f8fd56a73feb4718d4ee792c4914
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-03 17:48:50 +0800

    works

--- src/main/java/about_abstarct_class/WrittenResult.java
+++ src/main/java/about_abstarct_class/WrittenResult.java
@@ -2,5 +2,13 @@ package about_abstarct_class;
 
 
 public class WrittenResult {
+    private static double P1_WEIGHT = 0.4;
+    private static double P2_WEIGHT = 0.6;
+    private double score1, score2;
     
 }
+/*
+There are 2 papers
+Paper weighting is 40%
+Paper 2 weighting is 60%
+*/
