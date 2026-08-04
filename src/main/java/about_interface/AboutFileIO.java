c11468db64fd562a2b8be1be07b56d81cb3d25d1
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-04 17:44:51 +0800

    works

---
+++ src/main/java/about_interface/AboutFileIO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package about_interface;
+
+/**
+ *
+ * @author limis
+ */
+public class AboutFileIO {
+    public static void main(String[] argv){
+        
+    }
+    
+}
---
+++ src/main/java/about_interface/Invoice.java
@@ -0,0 +1,35 @@
+package about_interface;
+import java.util.ArrayList;
+
+
+
+public class Invoice {
+    private String code;
+    private int hours;
+    private double rate;
+    public Invoice(String code, int hours, double rate){
+        this.code = code;
+        this.hours = hours;
+        this.rate = rate;
+    }
+    public String getCode(){ return code; }
+    public int getHours(){ return hours; }
+    public double getRate(){ return rate; }
+    public double getTotal(){ return hours * rate; }
+    public String toString(){
+        return String.format("%s %d %.2f $%.2f", code, hours, rate, getTotal());
+    }
+    public static ArrayList<Invoice> getSample(){
+        ArrayList<Invoice> dummy = new ArrayList<>();
+        dummy.add(new Invoice("C501", 50, 25));
+        dummy.add(new Invoice("C301", 30, 35));
+        dummy.add(new Invoice("C101", 18, 60));
+        dummy.add(new Invoice("C202", 25, 25));
+        dummy.add(new Invoice("C401", 40, 50));
+        dummy.add(new Invoice("C303", 23, 35));
+        dummy.add(new Invoice("C701", 70, 40));
+        dummy.add(new Invoice("C109", 50, 25));
+        return dummy;
+    }
+}
+
