b79ff501e55785b08e06e7c4abc7b0dcb41e1814
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-04 16:39:38 +0800

    works

--- src/main/java/about_interface/RectCompare.java
+++ src/main/java/about_interface/RectCompare.java
@@ -3,5 +3,12 @@ package about_interface;
 
 
 public class RectCompare implements ICompare{
-    
+    public boolean compare(Object obj1, Object obj2){
+        Rectangle r1 = (Rectangle)obj1;
+        Rectangle r2 = (Rectangle)obj2;
+        if(r1.getArea() > r2.getArea())
+            return true; 
+        else 
+            return false;
+    }
 }
