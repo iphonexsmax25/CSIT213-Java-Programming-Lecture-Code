16b5a8cf1348976b813bf96acf91df7806e01238
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-04 16:19:12 +0800

    works

--- src/main/java/about_interface/InterfaceAndSorting.java
+++ src/main/java/about_interface/InterfaceAndSorting.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package about_interface;
 
-/**
- *
- * @author limis
- */
+
+
 public class InterfaceAndSorting {
       public static void main(String[] argv){
         int[] data = {80, 30, 50, 70, 20, 10};
