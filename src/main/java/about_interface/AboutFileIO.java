6b683eaaf49ad2d9771bf3b59e2ca1eef997e816
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-04 17:56:35 +0800

    works

--- src/main/java/about_interface/AboutFileIO.java
+++ src/main/java/about_interface/AboutFileIO.java
@@ -1,16 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package about_interface;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
-/**
- *
- * @author limis
- */
+
+
+
 public class AboutFileIO {
     public static void main(String[] argv){
-        
+        String filename = "invoice.txt";
+        Scanner reader = null;
+        try{
+            reader = new Scanner(new File(filename));
+            while(reader.hasNext()){
+                String oneLine = reader.nextLine();
+                System.out.println(oneLine);
+            }
+        }
+        catch(FileNotFoundException err){
+            System.out.println(err);
+        }
+        finally{
+            if (reader != null)
+                reader.close();
+        }
     }
     
 }
