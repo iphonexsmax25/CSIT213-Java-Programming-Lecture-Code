c75e81a635c162dd9c4c93896241dcb09255e4f6
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-11 18:27:11 +0800

    works

--- src/main/java/ReadWriteInvoice.java
+++ src/main/java/ReadWriteInvoice.java
@@ -1,58 +1,64 @@
 import java.util.*;
 import java.io.*;
 
+
+
 public class ReadWriteInvoice {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         readInvoice("InvoiceData.txt");
-    } 
-    public static void writeInvoie(ArrayList<invoice){
-        
     }
-    public static void readInvoice(String filename){
-        ArrayList<Invoice> data = new ArrayList<>();
-        Scanner reader = null;
-        try{
-            reader = new Scanner(new File(filename));
-            while (reader.hasNext()){
-                String oneLine = reader.nextLine();
-                String[] values = oneLine.split(",");
-                try{
-                    int hours = Integer.parseInt(values[1]);
-                    double rate = Double.parseDouble(values[2]);
-                    if(hours <= 0 || rate <= 0){
-                        // Throw Exception to interuptn this iteration
-                        throw new InvoiceDataException("Hours and Rate must be greater than 0");
-                        
-                    }
-                    Invoice newInvoice = new Invoice(values[0], hours, rate);
-                    if (data.contains(newInvoice)){
-                        throw new InvoiceDataException("Duplicate instance");
-                        
-                      // OK, add to ArrayList
-                      data.add(newInvoice);
-                    }
-                        
-                }
-                catch(NumberFormatException | InvoiceDataException err){
-                    System.out.printf(" [%s] %s\n",  oneLine, err);
-                    
-                }
-                
-            }
-            //After while loop
-            for(Invoice inv : data){
-                System.out.println(inv);
-                // Call writeInvoice to save to another life
-                writeInvoice(data,  "InvoiceData.txt");
+    public static void writeInvoice(ArrayList<Invoice> data, String filename) {
+        try (PrintWriter writer = new PrintWriter(filename)){ // inside round bracket --> everything opened inside the round brackets will be closed at the end of the try
+            writer.println("Processed Invoice Data");
+            // Save all instances in ArrayList to file
+            for (Invoice inv : data) {
+                writer.println(inv);
             }
         }
-        catch(FileNotFoundException err){
+        catch (FileNotFoundException err) {
             System.out.println(err);
         }
-        finally{
-            if(reader != null)
+    }
+    public static void readInvoice(String filename) {
+        ArrayList<Invoice> data = new ArrayList<>();
+        Scanner reader = null;
+        try { // do here?
+            reader = new Scanner(new File (filename)); // create a file instance based on the filename --> scanner reads from file instead of keyboard
+            while (reader.hasNext()) {
+                String oneLine = reader.nextLine();
+                String[] values = oneLine.split(",");
+                try {
+                    int hours = Integer.parseInt(values[1]);
+                    double rate = Double.parseDouble(values[2]);
+                    if (hours <= 0 || rate <= 0) {
+                        // throw Exception to interrupt this iteration
+                        throw new InvoiceDataException("Hours and Rate must be greater than 0");
+                    }
+                    Invoice newInvoice = new Invoice(values[0], hours, rate);
+                    if (data.contains(newInvoice)) {
+                        throw new InvoiceDataException(("Duplicate instance"));
+                    }
+                    // OK, add to ArrayList
+                    data.add(newInvoice);
+                }
+                catch (NumberFormatException | InvoiceDataException err) {
+                    System.out.printf("%s %s\n", oneLine, err);
+                }
+            }
+            // After while loop
+            for (Invoice inv : data) {
+                System.out.println(inv);
+            }
+            // Call writeInvoice to save to another file
+            writeInvoice(data, "invoice2_out.txt");
+        }
+        catch (FileNotFoundException err) {
+            System.out.println(err);
+        }
+        finally {
+            if (reader != null) {
                 reader.close();
+            }
         }
     }
-    
 }
