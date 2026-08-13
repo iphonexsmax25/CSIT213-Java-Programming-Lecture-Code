5799898889d018e42c9e7b5d44139a6b9304f22e
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-13 17:50:50 +0800

    works

---
+++ src/main/java/Invoice.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.io.Serializable;
+
+
+
+public class Invoice implements Serializable{
+    private String code;
+    private int hours;
+    private double rate;
+    public Invoice(String code, int hours, double rate){
+        this.code = code;
+        this.hours = hours;
+        this.rate = rate;
+    }
+    
+    
+    public boolean equals(Object other){
+        if(other == null)
+            return false;
+        if(other instanceof Invoice == false)
+            return false;
+        Invoice temp = (Invoice)other;
+        return code.equalsIgnoreCase(temp.code);
+    }
+            
+            
+    public String getCode(){ return code; }
+    public int getHours(){ return hours; }
+    public double getRate(){ return rate; }
+    public double getTotal(){ return hours * rate; }
+    public String toString(){
+        return String.format("%s %d %.2f $%.2f", code, hours, rate, getTotal());
+    }
+    public static ArrayList<Invoice> getSample(){
+        ArrayList<Invoice> dummy = new ArrayList<>();
+        dummy.add(new Invoice("C501", 50, 25));
+        dummy.add(new Invoice("C301", 30, 35));
+        dummy.add(new Invoice("C101", 18, 60));
+        dummy.add(new Invoice("C202", 25, 25));
+        dummy.add(new Invoice("C401", 40, 50));
+        dummy.add(new Invoice("C303", 23, 35));
+        dummy.add(new Invoice("C701", 70, 40));
+        dummy.add(new Invoice("C109", 50, 25));
+        return dummy;
+    }
+}
