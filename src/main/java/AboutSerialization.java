8522a8443d679ba780212709500f2b6b740d94e6
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-13 18:10:42 +0800

    works

--- src/main/java/AboutSerialization.java
+++ src/main/java/AboutSerialization.java
@@ -7,7 +7,24 @@ public class AboutSerialization {
 
     public static void main(String[] args) {
         saveInvoice("invoice_binary.txt");
+        readInvoice("invoice_binary.txt");
     }
+    public static void readInvoice(String filename){
+        try(ObjectInputStream reader =
+            new ObjectInputStream(new FileInputStream(filename))){
+
+        @SuppressWarnings("unchecked")
+        ArrayList<Invoice> data = (ArrayList<Invoice>) reader.readObject();
+
+        for (Invoice inv : data){
+            System.out.println(inv);
+        }
+
+    }   catch(ClassNotFoundException | IOException err){
+        System.out.println(err);
+    }
+}
+
     public static void saveInvoice(String filename){
         ArrayList<Invoice> data = Invoice.getSample();
         try(ObjectOutputStream writter = 
