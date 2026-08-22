1fc0e0ad0daac2ae663bb141947dcf5f50560d86
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-22 09:02:03 +0800

    works

--- src/main/java/com/mycpmpany/NumberApp.java
+++ src/main/java/com/mycpmpany/NumberApp.java
@@ -12,8 +12,26 @@ import java.util.Random;
 
 
 public class NumberApp extends Application{
+    private TextField txGuess, txHint;
+    private Button bnReset;
+    private int secretNumber;
+    private Random rNumber;
+    
+    
     public void start(Stage topWin){
+        rNumber = new Random();
+        createUI(topWin);
+        reset();
         
+    }
+    private void createUI(Stage topWin){
+        
+    }
+    private void reset(){
+        
+    }
+    public static void main(String[] args){
+        launch();
     }
     
 }
