86acb09da57a41e953240accc43380b2f16bf303
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-22 09:18:47 +0800

    works

--- src/main/java/com/mycpmpany/NumberApp.java
+++ src/main/java/com/mycpmpany/NumberApp.java
@@ -25,10 +25,27 @@ public class NumberApp extends Application{
         
     }
     private void createUI(Stage topWin){
-        
+        // GUI components
+        txGuess = new TextField();
+        txHint = new TextField();
+        bnReset = new Button("Reset");
+        // Layout manager 
+        GridPane lMgr = new GridPane();
+        lMgr.addRow(0, new Label());
+        lMgr.addRow(0, txGuess);
+        lMgr.addRow(1, new Label("Hint"));
+        lMgr.addRow(1, txHint);
+        lMgr.add(bnReset, 1, 2);
+        // Put the layout manager inside Scene
+        Scene onePage = new Scene(lMgr);
+        //Scene must go into a Sage
+        topWin.setScene(onePage);
+        topWin.show();
     }
     private void reset(){
-        
+        secretNumber = rNumber.nextInt(100);
+        txGuess.setText("");
+        txHint.setText("");
     }
     public static void main(String[] args){
         launch();
