85fea11a2052cf46aeb0b5727ecd15ce7cac0aaa
iphonexsmax25 <limisaac2022@gmail.com>
2026-08-22 10:10:55 +0800

    works

--- src/main/java/AboutOverloading.java
+++ src/main/java/AboutOverloading.java
@@ -5,5 +5,8 @@ public class AboutOverloading {
     public static void main (String args){
         
     }
+    public static void test(double value){
+        System.out.println("test(double) " + value);
+    }
     
 }
